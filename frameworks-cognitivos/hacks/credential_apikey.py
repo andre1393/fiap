d729c0c5a2b53fa6a56018efe32f894cc3324426
Andre Oliveira da Silva <andre_1393@hotmail.com>
2019-03-31 23:13:01 -0300

    create NLP directory

---
+++ frameworks-cognitivos/hacks/credential_apikey.py
@@ -0,0 +1,2 @@
+#Credential for Visual Recognitive
+visualrecognition_apikey='u-wtKNFlyFRyCLQbTqr05wBPeZ3H7h-ZuY3hCC3a1NSc'
---
+++ frameworks-cognitivos/hacks/hacks-cognitives-plataforms/python/visual_recognition/watson_recognition_general.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../../../')
+
+from credential_apikey import *
+import json
+
+from watson_developer_cloud import VisualRecognitionV3
+
+visual_recognition = VisualRecognitionV3(
+    '2018-03-19',
+    iam_apikey=visualrecognition_apikey)
+
+with open('../../datasets/images/lions/imagem_test1.jpg', 'rb') as images_file:
+    classes = visual_recognition.classify(
+        images_file,
+        threshold='0.6',
+	classifier_ids='default').get_result()
+print(json.dumps(classes, indent=2))
